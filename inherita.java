ca1e8969c9731eebb76f2da7ef50ec05fb95a52d
dukefelix <fhabimana534@gmail.com>
2023-11-09 11:00:54 +0200

    Create inherita.java
    first five teams in the  world

---
+++ inherita.java
@@ -0,0 +1,52 @@
+
+// java program to show first five teams using classes
+
+class A extends B implements E{
+	void printa() {
+		System.out.println("MANCHESTER CITY");
+	}
+	public void printe() {
+		 System.out.println("MANCHESTER UNITED");
+	}
+}
+class B extends C{
+	void printb() {
+		System.out.println("REAL MADRID");
+	}
+}
+class C extends D{
+	void printc() {
+		System.out.println("AJAX AMSTERDAM");
+	}
+}
+class D{
+	void printd() {
+		System.out.println("FC PORTO");
+	}
+}
+interface E{
+	void printe();
+}
+
+
+public class inherita {
+	public static void main(String[] args) {
+        A Obj = new A();
+        Obj.printa();
+        Obj.printb();
+        Obj.printc(); 
+        Obj.printd();
+        Obj.printe();
+	}
+
+}
+/*
+ * 
+output
+
+january
+february
+march
+April
+May
+ */
